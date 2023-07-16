22488efe1e8c3e0f3ba2e7520d22aa3db12257b7
kausgang <write.to.kaustav@gmail.com>
2023-07-16 00:12:01 -0400

    server ready

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # TestYourself
- 
+
+If you have a set of questions that you want to practice with
+
+### 1. Create Database
+
+Enter the Question with answers and reference (If any)
+
+### 2. Ask yourself random questions from the database you created
+
+Start the Random Question
+
+### 3. If you don't like the question or know the answer, skip it
+
+Change the question if you don't like it
+
+### 4. Answer the question in the space provided
+
+Answer descriptively or in one word
+
+### 5. Reveal the answer to see if you were right
+
+Use the Reveal Answer button
+
+## Future Scope
+
+1. Create a proper data Model and integrate with NoSql DB
+2. Integrate with ChatGPT
+3. Database managemet from UI
+4. Make it a microservice application
---
+++ server/routes/index.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var router = express.Router();
+
+var sqlite3 = require("sqlite3");
+const fs = require("fs");
+
+/* GET home page. */
+router.get("/showDBs", function (req, res, next) {
+  const files = fs.readdirSync("./public/DB");
+  res.send(files);
+});
+
+router.post("/createDB", function (req, res, next) {
+  //get the DBname from client application
+  let dbname = req.body.dbname;
+
+  // create the database and tables
+  let db = new sqlite3.Database("public/DB/" + dbname + ".db");
+  db.run(
+    "CREATE TABLE IF NOT EXISTS questions(ID INTEGER PRIMARY KEY AUTOINCREMENT,question,answer,reference)"
+  );
+  db.close();
+
+  res.sendStatus(200);
+});
+
+router.post("/addQuestion", function (req, res, next) {
+  //get the DBname from client application
+  let dbname = req.body.dbname,
+    question = req.body.question,
+    answer = req.body.answer,
+    reference = req.body.reference;
+
+  // create the database and tables
+  let db = new sqlite3.Database("public/DB/" + dbname + ".db");
+
+  let data = [question, answer, reference];
+  db.run(
+    `INSERT INTO questions (question,answer,reference) VALUES (?,?,?)`,
+    data,
+    function (err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      // get the last insert id
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+    }
+  );
+  db.close();
+
+  res.sendStatus(200);
+});
+
+router.get("/getQuestion", function (req, res, next) {
+  //the request has to be in the form -http://localhost:3000/getQuestion?dbname=salesforce
+
+  //get the DBname from client application
+  let dbname = req.query.dbname;
+
+  let db = new sqlite3.Database("public/DB/" + dbname + ".db");
+
+  let sql = `SELECT * FROM questions`;
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.send(rows);
+  });
+});
+module.exports = router;
